a7b46fc65ae3f39070dbb7a1f9f1b378463adbbc
Abdullah Motiwala <motiwalaabdullah100@gmail.com>
2026-01-04 20:59:52 +0500

    04-01-26: chap 6 completed

---
+++ js/app.js
@@ -0,0 +1,88 @@
+// Premitive Datatypes
+// Dynamic Typed Language
+var varString = "Abdullah";
+var varNumber = 600;
+var varBoolean = true;
+var varNull = null;
+var varUndefined = undefined;
+var varUsed = varNull;
+
+// Non Premitive Datatypes
+var varArray = ["asdf", 123, true];
+var varObject = { a: "abc" };
+
+// var 12alpha: Not allowed
+// var al12pha: allowed
+// var my name: Not allowed
+// var my_name: allowed
+// var my*/name: Not allowed
+// var my%name: Not allowed
+// var my$name: allowed
+// var var = "": Reserved keywords not allowed
+
+// var first_full_name: snake_case
+// var FirstFullName: PascalCase
+// var firstFullName: camelCase
+// var FIRST_FULL_NAME: SCREAMING_SNAKE_CASE
+
+// var firstName;
+// firstName = "Abdullah"
+// firstName = "Motiwala"
+
+// var firstName = "'Abdullah'"
+// console.log(firstName)
+
+// firstName = "Motiwala"
+// console.log(firstName)
+
+// var age = 25;
+// age = age + 1
+// console.log(age)
+
+// var newAge = 27
+// // newAge + 9
+// console.log(newAge)
+// var a = 4-2/1+1 :1
+// B O D M A S
+// Brackets-Open Division Mutliplication Addition Substraction
+
+// var equation = 12 + (62 / (3 + 1)) * 8 - (3 - 1) - 5;
+// var bracket1 = 4;
+// var bracket2 = 2;
+
+// var division = 62 / bracket1;
+// var multiplication = division * 8;
+// var addition = 12 + multiplication;
+// var subs1 = -bracket2 - 5;
+// var result = addition +subs1;
+// console.log(result)
+// console.log(equation)
+// var a = 1;
+// var b = 2 + a;
+
+// var a = 10;
+// var b = a % 2;
+// console.log(b)
+
+// var userNumber = 9198
+// var isEven = userNumber % 2
+// console.log(isEven)
+
+// alert(1916)
+
+var ourNum = 25;
+// value++
+// console.log(++value)
+// // value = value + 1
+// value *= 100
+// value--
+
+// console.log(value++)
+// console.log(value)
+
+// console.log(++value)
+// console.log(value)
+// var a = value++ + value++ + value++ + ++value
+// var a = ourNum++ + value++ + value++
+// a = 78 + 29 = 107
+// ourNum = 29
