e190b2263b327391e072c615a7445cd4dd655344
Abdullah Motiwala <motiwalaabdullah100@gmail.com>
2026-02-28 14:53:26 +0500

    28-02-26: team selection and coin selection completed

--- cricket-game/app.js
+++ cricket-game/app.js
@@ -1,14 +1,58 @@
+// Hoisting
+
 let userName = "";
+let selectedTeam = "";
+let selectedCoin = "";
 
 const nameWrapper = document.getElementById("name-wrapper");
-const nameInput = nameWrapper.querySelector("input");
 const nameNextButton = nameWrapper.querySelector("button");
+const nameInput = nameWrapper.querySelector("input");
+
+const teamWrapper = document.getElementById("team-wrapper");
+const teams = teamWrapper.getElementsByClassName("team");
+
+const tossWrapper = document.getElementById("toss-wrapper");
+const coins = tossWrapper.getElementsByClassName("coin");
 
 function getUserName() {
   const value = nameInput.value;
-  console.log("===run");
   userName = value;
-  console.log(userName);
+  nameWrapper.remove();
+  teamWrapper.classList.replace("none", "flex");
+}
+nameNextButton.addEventListener("click", getUserName);
+
+for (let i = 0; i < teams.length; i++) {
+  const team = teams[i];
+  team.addEventListener("click", selectTeam);
 }
 
-nameNextButton.addEventListener("click", getUserName);
+function selectTeam(event) {
+  selectedTeam = event.target.innerText;
+  const isConfirm = confirm(`Are you sure, You want to select ${selectedTeam}`);
+  if (isConfirm) proceedToNextPageThenSelecTeam();
+}
+
+function proceedToNextPageThenSelecTeam() {
+  teamWrapper.remove();
+  tossWrapper.classList.replace("none", "flex");
+}
+
+for (let i = 0; i < coins.length; i++) {
+  const coin = coins[i];
+  coin.addEventListener("click", selectCoin);
+}
+
+function selectCoin(event) {
+  selectedCoin = event.target.innerText;
+  const isConfirm = confirm(`Are you sure, You want to select ${selectedCoin}`);
+  if (isConfirm) proceedToNextPageThenSelectCoin();
+}
+
+function proceedToNextPageThenSelectCoin() {
+
+
+  
+  tossWrapper.remove();
+  // tossWrapper.classList.replace("none", "flex");
+}
