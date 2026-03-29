bd25c80f38137c7822149b320e4171691e64f06a
Abdullah Motiwala <motiwalaabdullah100@gmail.com>
2026-03-29 21:07:11 +0500

    half revision

---
+++ js-revision/app.js
@@ -0,0 +1,96 @@
+// alert("Hi");
+
+// Premitive DataTypes
+var firstName = "Abdullah";
+var age = 24;
+var isParent = false;
+var netWorth = undefined;
+var medicine = null;
+
+// var newAge = age + 25;
+age = age + 25;
+age += 25;
+age++;
+++age;
+
+// var newAge = 5 + 6 - "10" + "1"
+var newAge = 5 + 6 - 10 + 1;
+var isEven = 10 % 2;
+console.log(newAge);
+
+var firstName = "Motiwala";
+firstName = "Mohd. Abdullah";
+
+// Non Premitive DataTypes
+var symptoms = ["ghootne me dard", "sar me dard"];
+var lastReport = {
+  homoglobin: 140,
+  platellets: "1lac50hazar",
+};
+
+// var first name : Space not allowed
+// var 1stname : start with number not allow, number in between or at at allowed though
+// var user&name : special characters other than _ and $ are not allowed
+// var var : any reserved keyword of js is not allowed
+// var first+last any mathematical sign is not allowed
+
+// var a = 1
+// a--
+// --a
+// console.log(++a)
+// console.log(a)
+var a = 1;
+const result = a + a++ + ++a + a++ + a + ++a;
+console.log(result);
+// 1 + 1 + 3+3+4 + 5
+
+// var answer = prompt("How're you?","Fine") //always return data in string;
+
+// console.log(answer)
+
+// const isRain = false;
+// if (isRain == true) {
+//   console.log("Keep Umbrella");
+// }
+
+const isTandoorOpen = false;
+const isBakeryOpen = true;
+
+// if (isTandoorOpen == true) {
+//   console.log("Buy Tandoor-Roti");
+// } else if (isBakeryOpen == true) {
+//   console.log("Buy Double-Roti");
+// } else {
+//   console.log("Buy Chapati");
+// }
+// const con = isTandoorOpen == true
+// console.log(con)
+
+// if (!isTandoorOpen) {
+//   console.log("Buy Tandoor-Roti");
+// } else if (isBakeryOpen) {
+//   console.log("Buy Double-Roti");
+// } else {
+//   console.log("Buy Chapati");
+// }
+
+
+// ===, == 
+// <, >, <=, >=
+
+// const age = 24
+// if(age<=30){
+
+// }
+
+// if (age>=20){
+
+// }
+
+// if([1,2] == [1,2]){
+
+// }
+
+// if(age === "24"){
+
+// }
